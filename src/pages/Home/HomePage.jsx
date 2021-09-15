2de635b3674ce16ef396aee68573c0147e6fb63b
puneetsingh99 <99singhpuneet99@gmail.com>
2021-09-15 12:02:20 +0530

    refactor: rename Home page

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,31 @@
+import { NavBar } from "../../components";
+import { VideoList } from "../../components/VideoList/VideoList";
+import { VideoListHeader } from "../../components/VideoList/VideoListHeader";
+import { SideBar } from "./components/SideBar/SideBar";
+import "./home-page-styles.css";
+import { useVideos } from "../../contexts";
+import { toast } from "react-toastify";
+import { toastConfig } from "../../utils/toastConfig";
+import { Loader } from "../../components/Loader";
+
+export const Home = () => {
+  const { filteredVideos, allVideos } = useVideos();
+  const { status, error } = allVideos;
+
+  return (
+    <>
+      <NavBar />
+      <main className={`homepage-main`}>
+        <SideBar />
+        <div className={`video-container`}>
+          <VideoListHeader />
+          {status === "loading" && <Loader />}
+          {status === "succeeded" && (
+            <VideoList filteredVideos={filteredVideos} />
+          )}
+          {status === "error" && toast.error(error, toastConfig)}
+        </div>
+      </main>
+    </>
+  );
+};
