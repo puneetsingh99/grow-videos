267c94e6b21ee6a1b87ba0fcdd843703fa52428a
puneetsingh99 <99singhpuneet99@gmail.com>
2021-09-15 12:21:54 +0530

    chore: fix netlify build error

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { HomePage } from "./home/HomePage";
-export { VideoPlayer } from "./play-video/VideoPlayer";
-export { Login } from "./private/login/Login";
-export { Signup } from "./private/signup/Signup";
