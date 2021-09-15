ee666edb802a415786a3512e5a7bdd41dcfe2063
puneetsingh99 <99singhpuneet99@gmail.com>
2021-09-15 06:33:09 +0530

    chore: change server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
-const API_ROOT = `http://localhost:5000`;
-// const API_ROOT = `https://salty-savannah-53232.herokuapp.com`;
+// const API_ROOT = `http://localhost:5000`;
+const API_ROOT = `https://salty-savannah-53232.herokuapp.com`;
 
 export const API_ALL_VIDEOS = `${API_ROOT}/videos`;
 export const apiGetVideo = (videoId) => `${API_ALL_VIDEOS}/${videoId}`;
@@ -8,23 +8,3 @@ export const API_LOGIN = `${API_ROOT}/login`;
 export const API_SIGNUP = `${API_ROOT}/user`;
 export const API_SEARCH = `${API_ROOT}/videos/search`;
 export const apiGetUser = (userId) => `${API_ROOT}/user/${userId}`;
-
-//addToPlaylist, removeFromPlaylist
-// {
-//     "videoId": "",
-//     playlistName: ""
-// }
-
-//createPlaylist, removePlaylist
-// {
-//     playlistName: ""
-// }
-
-// 6116d3624bbdb52668515dba
-// 6116d39d4bbdb52668515dbd
-// 6116d3ab4bbdb52668515dbf
-// 6116d3b64bbdb52668515dc1
-// 6116d3c24bbdb52668515dc3
-// 6116d3cd4bbdb52668515dc5
-// 611c203f6ea28120d05a2ad2
-// 611c21c86ea28120d05a2ad8
